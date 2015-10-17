e3b08cd56cba28a2c0c7aa29142cf897705f07b9
jfrey <nomhad@gmail.com>
2015-10-17 04:10:48 -0400

    Skip empty commit for png.

--- makepng.sh
+++ makepng.sh
@@ -4,5 +4,9 @@ while IFS='' read -r line ;  do
     $((++i))
     printf -v num '%07d' $i
     echo "Converting hash: $line --> ${num}.png"
-    cutycapt --url=file://$(pwd)/${line}.html --out=${num}.png 
+    if [ -s  ${line} ] ; then
+       cutycapt --url=file://$(pwd)/${line}.html --out=${num}.png 
+    else
+        echo "empty file, skip."
+    fi ;
 done < "$1"
