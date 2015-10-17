9e21bc1047d83ec46a7b07479db72f654b507bd8
jfrey <nomhad@gmail.com>
2015-10-17 03:57:20 -0400

    Frame number instead of hash for png filename.

--- makepng.sh
+++ makepng.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+i=0
 while IFS='' read -r line ;  do
-    echo "Converting hash: $line"
-    cutycapt --url=file://$(pwd)/${line}.html --out=${line}.png 
+    $((++i))
+    printf -v num '%07d' $i
+    echo "Converting hash: $line --> ${num}.png"
+    cutycapt --url=file://$(pwd)/${line}.html --out=${num}.png 
 done < "$1"
