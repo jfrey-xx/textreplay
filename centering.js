0a3187db9b143426b1ae089c0a869240e18620ac
jfrey <nomhad@gmail.com>
2016-08-11 14:29:19 +0200

    Fixing size for big documents.

--- centering.js
+++ centering.js
@@ -2,10 +2,12 @@
 // once document is loaded
 $( document ).ready(function() {
   // document width
-  body_width = $("body").width()
+  body_width = $("body").width();
+  // column width from CSS
+  column_width = 260;
   // using pos to guess width of actual content
-  content_width = $("pre").position().left*2
-  margin = (body_width - content_width)/2
+  content_width = $("pre").position().left*2 + column_width;
+  margin = (body_width - content_width)/2;
 
   // centering horizontally
   $(".b9").css("margin-left", margin);
