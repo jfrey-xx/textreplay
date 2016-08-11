3d7493563cf034d98b82868ff4fb905091241798
jfrey <nomhad@gmail.com>
2016-08-11 13:55:28 +0200

    Fix document width.

--- centering.js
+++ centering.js
@@ -1,8 +1,14 @@
 
 // once document is loaded
 $( document ).ready(function() {
+  // document width
+  body_width = $("body").width()
+  // using pos to guess width of actual content
+  content_width = $("pre").position().left*2
+  margin = (body_width - content_width)/2
 
   // centering horizontally
-  $(".b9").css("margin-left", (($("body").width() - $("pre").position().left)*2)/2);
-
+  $(".b9").css("margin-left", margin);
+  // reducing document width as much
+  $("body").css("width", body_width-margin);
 });
