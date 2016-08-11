a3f491bc1a98a306b0446db48b420f730e2a6d09
jfrey <nomhad@gmail.com>
2016-08-11 15:48:53 +0200

    Conversion to png working from html files directly

--- makepng.sh
+++ makepng.sh
@@ -3,7 +3,7 @@ i=0
 while IFS='' read -r line ;  do
     printf -v num '%07d' $i
     echo "Converting hash: $line --> ${num}.png"
-    if [ -s  ${line} ] ; then
+    if [ -s  ${line}.html ] ; then
        i=$((i+1))
        cutycapt --min-width=7680 --min-height=4320 --url=file://$(pwd)/${line}.html --out=${num}.png 
     else
