4eba0953ed61f280cb7eb113b07e1f32a85add1f
jfrey <nomhad@gmail.com>
2016-08-12 10:56:25 +0200

    Fonts must be installed on system for cutycapt.

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ Make a video out of a text versioned with git. Based on [git-playback](https://g
 
 Tested on kubuntu 16.04
 
+# Misc
+
+For cutycapt to work well with font, install them on the system. E.g. copy ttf files in "/usr/share/fonts/truetype/custom/" and run `sudo fc-cache -fv`.
--- centering.js
+++ centering.js
@@ -1,6 +1,17 @@
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+
 // once document is loaded
 $( document ).ready(function() {
+
   // document width
   body_width = $("body").width();
   // column width from CSS
@@ -13,4 +24,5 @@ $( document ).ready(function() {
   $(".b9").css("margin-left", margin);
   // reducing document width as much
   $("body").css("width", body_width-margin);
+
 });
